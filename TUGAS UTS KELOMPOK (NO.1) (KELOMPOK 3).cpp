85c67372df6a364a70e126d4617d798ccba461ba
ivonfadilah <ivnfdlhkrtka@gmail.com>
2024-11-02 16:26:17 +0700

    Add files via upload

---
+++ TUGAS UTS KELOMPOK (NO.1) (KELOMPOK 3).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string nama;
+    int golongan, jamkerja, upahperjam, totalgaji, lembur;
+
+    cout << "Masukkan nama Anda: ";
+    cin >> nama;
+    cout << "Masukkan Golongan Anda: ";
+    cin >> golongan;
+    cout << "Masukkan jam kerja dan per minggu: ";
+    cin >> jamkerja;
+
+    if (golongan == 1) {
+        upahperjam = 25000;
+    }
+    else if (golongan == 2) {
+        upahperjam = 35000;
+    }
+    else if (golongan == 3) {
+        upahperjam = 50000;
+    }
+    else {
+        cout << "Golongan tidak valid!" << endl;
+        return 1;
+    }
+
+    totalgaji = jamkerja * upahperjam;
+
+    if (jamkerja > 48) {
+        lembur = (jamkerja - 48) * 10000;
+    }
+
+    totalgaji += lembur;
+    cout << "\nTotal Gaji " << nama << " per minggu : " << totalgaji << endl;
+
+    return 0;
+}
